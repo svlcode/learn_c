8d354c343437fa62dba5d9857f99deb46c04357d
Vio <vio.slaniceanu@gmail.com>
2024-04-02 21:23:39 +0300

    fix read_integer bug

--- Basics/Numbers/read_integer.c
+++ Basics/Numbers/read_integer.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <conio.h>
+#include <limits.h>
 
 int read_integer()
 {
+  // const int max_int = 2147483647; // Maximum value for a 32-bit signed integer
   int number = 0;
   while (1)
   {
@@ -13,18 +15,19 @@ int read_integer()
       break;
     }
 
-    // todo fix bug: when number is too big and you remove digits
-    // only read the digit if the first digit is not zero
     if (isdigit(ch))
     {
-      if (number == 0 && ch == '0')
+      if (number == 0 && ch == '0') // only read the digit if the first digit is not zero
         printf("\b \b", ch);
       else
       {
         int digit = ch - '0'; // convert it to a number
-
-        printf("%c", ch);               // show it to the screen
-        number = (number * 10) + digit; // add it to the number
+        long long temp_result = ((long long)number * 10) + digit;
+        if (temp_result <= INT_MAX && temp_result >= INT_MIN) // check if the number overlows int max value
+        {
+          printf("%c", ch);     // show it to the screen
+          number = temp_result; // add it to the number
+        }
       }
     }
     else if (ch == '\b' && number != 0)
