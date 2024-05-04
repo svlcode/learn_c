5c55e883ea6eea24c0f4125dd27ae63e18b9193a
Vio <vio.slaniceanu@gmail.com>
2024-05-04 21:14:15 +0300

    update pointer_arithmetic.c

--- Basics/Pointers/pointer_arithmetic.c
+++ Basics/Pointers/pointer_arithmetic.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+// in this situation substractic 2 pointers will result in calculating
+// how many <double> locations are between them
+// this will be 1000 because, size of double is 8 and then
+// 2000 + 8*1000 = 10000
+void substracting_pointers()
+{
+  double *p = (double *)10000;
+  double *q = (double *)2000;
+
+  // p - q = (10000 - 2000) / 8
+  printf("p - q = %d\n", p - q);
+}
+
 int main()
 {
   int a = 10;
@@ -16,8 +29,10 @@ int main()
   char *p0;
   p0 = (char *)p; // typecasting
 
-  printf("Address of p0 = %d, value = %d", p0, *p0);
+  printf("Address of p0 = %d, value = %d\n", p0, *p0);
   // value will be 1 because
   // 1025 = 00000000 00000000 00000100 00000001
   // and p0 points only to the first byte
+
+  substracting_pointers();
 }
